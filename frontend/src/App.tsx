772dc9559d71860ce2b5c6a59aa6ee3a0765d64c
HenriqueMVSS <henrique_mateus1994@hotmail.com>
2022-07-15 02:02:50 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   
   return (
     <>
-      <h1>Hello World!</h1>
-      <NotificationButton />
+      <Header/>
+      {/* <NotificationButton /> */}
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
